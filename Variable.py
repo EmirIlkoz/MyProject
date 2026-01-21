120cd7236ffa13ef10a866430a923c40a896badc
EmirIlkoz <emirilkoz@hotmail.com>
2026-01-21 19:31:54 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,16 @@
+x = 5
+y = 10
+sum = x + y
+print("The sum of x and y =", sum)
+name = input ("What is your name?")
+print("Hello", name)
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+total = a + b
+minus = a - b
+multiply = a * b
+divide = a / b
+print("The total of a and b", total)
+print("The difference of a and b", minus)
+print("The product of a and b", multiply)
+print("The division of a and b", divide)
