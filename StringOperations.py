8c777882ceb28280638b8f1f830354edf47f39cf
EmirIlkoz <emirilkoz@hotmail.com>
2026-01-28 19:46:50 +0300

    Add files via upload

---
+++ StringOperations.py
@@ -0,0 +1,13 @@
+print("String Operations")
+str1 = "hello"
+str2 = "awesome"
+str3 = str(input("Enter the string"))
+str4 = "CAPITAL"
+print(str3)
+revText = str3[::-1]
+str3 = revText
+print(str3)
+print(str1 + str2)
+print(str1.upper())
+print(str4.lower())
+print(len(str3))
