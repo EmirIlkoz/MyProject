9455dca0f4e0a566d45abef4eac3c842e033ce77
EmirIlkoz <emirilkoz@hotmail.com>
2026-01-28 19:29:21 +0300

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,17 @@
+print("Typecasting")
+a = 6
+b = 2.3
+c = "Hello"
+d = True
+print("Data type of a =", type(a))
+print("Data type of b =", type(b))
+print("Data type of c =", type(c))
+print("Data type of d =", type(d))
+a = float(a)
+b = int(b)
+print("After typecasting")
+print("Data type of a =", type(a))
+print("Data type of b =", type(b))
+a = str(a)
+print("Data type of a =", type(a))
+print("A is", a)
